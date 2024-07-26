afbc92702b3f38174204a32ba18a101f03a9d427
prasanth0402 <prasanth1359d@gmail.com>
2024-07-26 10:56:56 +0530

    auth controll added in backend

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { login, logout, signup } from "../controllers/authController.js";
+import { gteMe, login, logout, signup } from "../controllers/authController.js";
+import { protectRoute } from "../middleware/protectRoute.js";
 
 const router = express.Router();
 
+router.get("/me", protectRoute, gteMe);
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,16 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/authRoutes.js";
 import connectMongodb from "./db/connectMongodb.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json()); // to pass req.body using postman
+app.use(express.urlencoded({ extended: true })); // to pass the data using postmen
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
